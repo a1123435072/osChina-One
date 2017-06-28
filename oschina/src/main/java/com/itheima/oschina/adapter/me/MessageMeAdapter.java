d6e04bd2dbab13318681d4807c61ed24ee1aaf4e
huaqing <1123435072@qq.com>
2017-06-28 20:39:00 +0800

    我的我的界面中消息界面的创建

---
+++ oschina/src/main/java/com/itheima/oschina/adapter/me/MessageMeAdapter.java
@@ -0,0 +1,48 @@
+package com.itheima.oschina.adapter.me;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.itheima.oschina.R;
+
+/**
+ * Created by yangg on 2017/6/28.
+ */
+
+public class MessageMeAdapter  extends RecyclerView.Adapter {
+    private Context context;
+
+    public MessageMeAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(context).inflate(R.layout.message_me_item, parent, false);
+
+        return new viewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        public viewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- oschina/src/main/java/com/itheima/oschina/fragment/MeMessageFragment/MessageMeFragment.java
+++ oschina/src/main/java/com/itheima/oschina/fragment/MeMessageFragment/MessageMeFragment.java
@@ -1,15 +1,29 @@
 package com.itheima.oschina.fragment.MeMessageFragment;
 
+import android.app.FragmentBreadCrumbs;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.itheima.oschina.R;
+import com.itheima.oschina.adapter.me.MessageMeAdapter;
+import com.itheima.oschina.adapter.opensoft.recommendAdapter;
+import com.itheima.oschina.view.RecycleViewDivider;
+import com.jcodecraeer.xrecyclerview.ProgressStyle;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
-import static android.R.attr.fragment;
+import org.senydevpkg.net.HttpHeaders;
+import org.senydevpkg.net.HttpParams;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by yangg on 2017/6/27.
@@ -17,10 +31,14 @@ import static android.R.attr.fragment;
 
 public class MessageMeFragment extends Fragment {
 
+
+    private XRecyclerView rv_message;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.message_fragment, container, false);
+
         return view;
 
     }
@@ -28,6 +46,56 @@ public class MessageMeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        rv_message = (XRecyclerView) view.findViewById(R.id.rv_messageMe);
+
+        initRecycleView();
     }
+
+    /**
+     * 初始化Recycleview
+     */
+    private void initRecycleView() {
+        rv_message.addItemDecoration(new RecycleViewDivider(getActivity(),LinearLayoutManager.HORIZONTAL
+        ,1,Color.GRAY));
+        rv_message.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv_message.setRefreshProgressStyle(ProgressStyle.LineScalePulseOut);
+
+        rv_message.setPullRefreshEnabled(true);
+        rv_message.setLoadingMoreEnabled(true);
+
+        rv_message.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+
+
+
+            @Override
+            public void onLoadMore() {
+                refreshData();
+            }
+        });
+
+        rv_message.refresh();
+        MessageMeAdapter messageMeAdapter = new MessageMeAdapter(getActivity());
+        rv_message.setAdapter(messageMeAdapter);
+    }
+
+    //请求网络刷新数据的放法
+    private void refreshData() {
+        String url ="";
+
+        HttpHeaders headers = new HttpHeaders();
+
+       // headers.put()
+
+        HttpParams httpParams = new HttpParams();
+        httpParams.put("url",url);
+        //httpParams.put();
+
+    }
+
+
 
 }
