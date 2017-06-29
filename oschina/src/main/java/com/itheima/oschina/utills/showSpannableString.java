6f72def775d9aa205e03166027cf958469ce7790
xxm <xxmcell@sina.com>
2017-06-29 10:05:02 +0800

    gridlayout只有长按事件没有完成,轮播图bug没有解决,自定义webView没有完成

---
+++ oschina/src/main/java/com/itheima/oschina/utills/showSpannableString.java
@@ -0,0 +1,8 @@
+package com.itheima.oschina.utills;
+
+/**
+ * Created by jason on 2017/6/28.
+ */
+
+public class showSpannableString {
+}
---
+++ oschina/src/main/java/com/itheima/oschina/view/MyOsChinaWebView.java
@@ -0,0 +1,8 @@
+package com.itheima.oschina.view;
+
+/**
+ * Created by jason on 2017/6/28.
+ */
+
+public class MyOsChinaWebView {
+}
